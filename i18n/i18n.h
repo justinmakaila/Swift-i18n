1219f109c1effc85b2a3074b1aa1559eab726682
Justin Makaila <justinmakaila@gmail.com>
2016-01-12 15:36:55 -0500

    Initial Commit

---
+++ i18n/i18n.h
@@ -0,0 +1,19 @@
+//
+//  i18n.h
+//  i18n
+//
+//  Created by Justin Makaila on 1/12/16.
+//  Copyright © 2016 Wellth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for i18n.
+FOUNDATION_EXPORT double i18nVersionNumber;
+
+//! Project version string for i18n.
+FOUNDATION_EXPORT const unsigned char i18nVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <i18n/PublicHeader.h>
+
+
